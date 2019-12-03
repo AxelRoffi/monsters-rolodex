5ee6dc7356cce8ea6a477bc5df6b50d4e0650c59
Axel Roffi <axel.roffi@gmail.com>
2019-12-03 16:43:45 +0100

    Updates

---
+++ precache-manifest.5480c88ca6c9a8b5877b4b3fa6b100ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8c19bf0aefa00b7c469ba4dab086ae",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "512e4d63c944344cc783",
+    "url": "/monsters-rolodex/static/css/main.2586a22a.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "512e4d63c944344cc783",
+    "url": "/monsters-rolodex/static/js/main.bd159ac2.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
