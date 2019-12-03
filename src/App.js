e132dacb5e1e1fd844ab553801dee2795c040083
Axel Roffi <axel.roffi@gmail.com>
2019-12-03 16:44:42 +0100

    Adding files for github pages

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className='App'>
+      <h1>Monsters Rolodex</h1>
         <SearchBox
         placeholder='search monster'
         handleChange = {this.handleChange} />
